e2af118e458393e8e90b54dc26365476b57339e9
tima20 <tima20@vt.edu>
2025-04-25 14:38:10 -0400

    Completed compare method for File and moved ComparisonResult enum inside of File class

--- file.py
+++ file.py
@@ -6,79 +6,86 @@ from enum import Enum
 
 
 class File:
-    def __init__(self, abs_path=Path):
+    class ComparisonResult(Enum):
+        MATCH = 0  # Same path, name, and content
+        DIFF = 1  # Same path and name, different content
+        CONTENT_NAME_DUP = 2  # Same content and name, different path
+        CONTENT_PATH_DUP = 3  # Same content and path, different name
+        NAME_DUP = 4  # Same name, different content and path
+        CONTENT_DUP = 5  # Same content, different name and path
+        UNIQUE = 6  # No other file has this name and content (might share path)
+
+    def __init__(self, abs_path: Path):
         self.name = abs_path.name
-        self.path = abs_path.parent
+        self.dir_path = abs_path.parent
+        self.abs_path = abs_path
         self.size = abs_path.stat().st_size
         self.quick_hash = None
         self.full_hash = None
 
     def __str__(self):
-        return (
-            f"File: {self.name}\n"
-            f"\tPath: {self.path}\n"
-            f"\tSize: {self.size}\n"
-            f"\tHash: {self.hash}"
+        msg = (
+            f"File: {self.name}\n" f"\tPath: {self.dir_path}\n" f"\tSize: {self.size}\n"
         )
+        if hasattr(self, "quick_hash"):
+            msg += f"\tQuick Hash: {self.quick_hash}\n"
+        if hasattr(self, "full_hash"):
+            msg += f"\tFull Hash: {self.full_hash}\n"
+
+        return msg
 
     def compare(self, other: "File"):
-        # Check basics
+        # Compare traits
         same_name = self.name == other.name
-        same_path = self.path == other.path
-
-        # Content compare
+        same_path = self.dir_path == other.dir_path
         same_content = self.compare_content(other)
+
+        # Assign comparison type
+        if same_name and same_path and same_content:
+            return self.ComparisonResult.MATCH
+        if same_name and same_path and not same_content:
+            return self.ComparisonResult.DIFF
+        if same_content and same_name and not same_path:
+            return self.ComparisonResult.CONTENT_NAME_DUP
+        if same_content and same_path and not same_name:
+            return self.ComparisonResult.CONTENT_PATH_DUP
+        if same_name and not same_path and not same_content:
+            return self.ComparisonResult.NAME_DUP
+        if same_content and not same_path and not same_name:
+            return self.ComparisonResult.CONTENT_DUP
+        return self.ComparisonResult.UNIQUE
 
     def compare_content(self, other: "File"):
         # Quick size check
-        if self.size == other.size:
-            # Ensure the files have been quick hashed
-            self.quick_hash = self.quick_hash or self.__create_quick_hash()
-            other.quick_hash = other.quick_hash or other.__create_quick_hash()
+        if self.size != other.size:
+            return False
 
-            if self.quick_hash == other.quick_hash:
-                # Ensure the files have been fully hashed
-                self.full_hash = self.full_hash or self.__create_full_hash()
-                other.full_hash = other.full_hash or other.__create_full_hash()
+        # Quick hash comparison (about 4KB)
+        if not hasattr(self, "quick_hash"):
+            self.quick_hash = self.__create_quick_hash()
+        if not hasattr(other, "quick_hash"):
+            other.quick_hash = other.__create_quick_hash()
+        if self.quick_hash != other.quick_hash:
+            return False
 
-                if self.full_hash == other.full_hash:
-                    return True
-        return False
+        # Full hash comparison
+        if not hasattr(self, "full_hash"):
+            self.full_hash = self.__create_full_hash()
+        if not hasattr(other, "full_hash"):
+            other.full_hash = other.__create_full_hash()
+        if self.full_hash != other.full_hash:
+            return False
 
-    # Given two file paths, check if they contain the same content
-    def check_file_diff(file_path_A, file_path_B):
-        diff_log = None
-        if not filecmp.cmp(file_path_A, file_path_B, shallow=False):
-            with open(file_path_A) as base:
-                base_content = base.readlines()
-            with open(file_path_B) as comp:
-                comp_content = comp.readlines()
-
-            diff_log = list(
-                difflib.unified_diff(
-                    base_content, comp_content, file_path_A, file_path_B
-                )
-            )
-        return diff_log
+        return self.full_hash == other.full_hash
 
     def __create_quick_hash(self, chunk_size=4096):
-        with open(self.path, "rb") as file:
+        with open(self.abs_path, "rb") as file:
             fingerprint = file.read(chunk_size)
         return hashlib.md5(fingerprint).hexdigest()
 
     def __create_full_hash(self, algorithm="sha256", chunk_size=8192):
         hasher = hashlib.new(algorithm)
-        with open(self.path, "rb") as file:
+        with open(self.abs_path, "rb") as file:
             while chunk := file.read(chunk_size):
                 hasher.update(chunk)
         return hasher.hexdigest()
-
-
-class ComparisonRes(Enum):
-    MATCH = "match"
-    DIFF = "diff"
-    CONTENT_NAME_DUP = "content-name dup"
-    CONTENT_PATH_DUP = "content-path dup"
-    NAME_DUP = "name dup"
-    CONTENT_DUP = "content dup"
-    UNIQUE = "unique"
