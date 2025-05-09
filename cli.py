1d191e26ce66bb0e7624173ba4ba2dbd4d7c328a
tima20 <tima20@vt.edu>
2025-05-09 14:55:16 -0400

    Added "prompt_input_dirs" to cli as part of REPL methods. Main needs to be modified to incorporate the new func

--- cli.py
+++ cli.py
@@ -24,18 +24,6 @@ class UserOptionValidator(Validator):
                 f"Value {user_input} is out of range. Must be between 1 and {len(self.num_options)}"
             )
 
-class DirValidator(Validator):
-    def validate(self, document):
-        path = Path(document.text)
-        if not path.exists():
-            raise ValidationError(
-                message="Path does not exist. Please provide a valid path",  cursor_position=len(document.text)
-            )
-        elif not path.is_dir():
-            raise ValidationError(
-                message="Path is not a directory. Please provide a path to a directory",  cursor_position=len(document.text)
-            )
-
 
 def prompt_user_options(msg: str, options: List[str]) -> int:
     while True:  # Loop until valid input or exit
@@ -46,14 +34,67 @@ def prompt_user_options(msg: str, options: List[str]) -> int:
                 print(f"\t{i + 1}) {option}")
 
             # Gather user input and validate
-            user_input = int(prompt(">>> ", validator=UserOptionValidator(num_options=len(options))))
-
+            user_input = int(
+                prompt(">>> ", validator=UserOptionValidator(num_options=len(options)))
+            )
             return user_input - 1  # Adjust to 0-based indexing
-        except (ValueError, ValidationError) as e:
+        except ValidationError as e:
             print(f"\nError: {e}")
-        except EOFError:
-            print("\nEOF recieved. Exiting")
-            exit(0)
+
+
+class InputDirValidator(Validator):
+    def __init__(self):
+        self.keywords = ["done", "list"]
+
+    def validate(self, document):
+        user_input = document.text
+        # Return early if keyword
+        if user_input in self.keywords:
+            return
+        # Check if path leads to valid dir
+        path = Path(user_input)
+        if not path.exists():
+            raise ValidationError(
+                message="Path does not exist. Please provide a valid path",
+                cursor_position=len(document.text),
+            )
+        elif not path.is_dir():
+            raise ValidationError(
+                message="Path is not a directory. Please provide a path to a directory",
+                cursor_position=len(document.text),
+            )
+
+
+def prompt_input_dirs():
+    """Prompt the user for any number of input dirs to add to the index"""
+    input_dirs = []
+    validator = InputDirValidator()
+
+    # Loop until user enters 'done'
+    while True:
+        print(
+            "Enter the path to the input directory (e.g., /home/user/documents/project):\n",
+            "Type 'done' when finished or 'list' to view all added directories\n",
+        )
+        try:
+            user_input = prompt(">>> ", validator)
+            if user_input in validator.keywords:  # Handle keywords
+                match user_input:
+                    case "done":
+                        if not input_dirs:
+                            print("At least one input directory must be given")
+                        else:
+                            return input_dirs
+                    case "list":
+                        msg = ["Added directories: "]
+                        for i, dir in enumerate(input_dirs):
+                            msg.append(f"\t{i}) {dir}")
+                        print("\n".join(msg))
+            else:  # Handle paths
+                input_dirs.append(user_input)
+        except ValidationError as e:
+            print(f"\nError: {e}")
+
 
 def display_files(msg, file_list: List[File]):
     msg = [msg]
